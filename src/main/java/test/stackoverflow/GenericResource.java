9533ac765dad8aa529a10270244cae0dd7c41ca7
bitter_fox <bitterfoxc@gmail.com>
2015-04-24 15:40:54 +0900

    Initial Commit

---
+++ src/main/java/test/stackoverflow/GenericResource.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.stackoverflow;
+
+import javax.enterprise.context.RequestScoped;
+import javax.mvc.Controller;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+/**
+ * REST Web Service
+ *
+ * @author bitter_fox
+ */
+@Path("")
+@RequestScoped
+public class GenericResource {
+    @GET
+    @Controller
+    public String root() {
+        return "hello.jsp";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/hello.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : hello
+    Created on : 2015/04/24, 15:30:55
+    Author     : bitter_fox
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
